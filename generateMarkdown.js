ecd87a919be04b0cbe605f2b0d45243b83ac56ee
Carolina R <carolinarizk@gmail.com>
2021-02-04 09:42:11 -0500

    pasted a switch template

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  switch(expression) {
+  switch (expression) {
     case x:
       // code block
       break;
@@ -9,7 +9,7 @@ function renderLicenseBadge(license) {
       // code block
       break;
     default:
-      // code block
+    // code block
   }
 }
 
